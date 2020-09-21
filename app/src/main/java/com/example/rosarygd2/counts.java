0285791ee846370dcb78ef14b0281ffc876aa5b6
gedundrakpa <dgedun@hotmail.com>
2020-09-21 11:15:31 +0530

    2nd commit
    changed the ui of mani
    
    pics of ui added

--- app/src/main/java/com/example/rosarygd2/counts.java
+++ app/src/main/java/com/example/rosarygd2/counts.java
@@ -15,14 +15,14 @@ public class counts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counts);
 
-        CardView card1=findViewById(R.id.card1);
-        CardView card2=findViewById(R.id.card2);
-        CardView card3=findViewById(R.id.card3);
+       CardView card1=findViewById(R.id.cardView);
+       CardView card2=findViewById(R.id.cardView2);
+       /* CardView card3=findViewById(R.id.card3);
         CardView card4=findViewById(R.id.card4);
         CardView card5=findViewById(R.id.card5);
         CardView card6=findViewById(R.id.card6);
         CardView card7=findViewById(R.id.card7);
-        CardView card8=findViewById(R.id.card8);
+        CardView card8=findViewById(R.id.card8);*/
 
 
         card1.setOnClickListener(new View.OnClickListener() {
@@ -38,8 +38,12 @@ public class counts extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent=new Intent(counts.this,about1.class);
+                startActivity(intent);
+
             }
         });
+        /*
         card3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,7 +80,7 @@ public class counts extends AppCompatActivity {
 
             }
         });
-
+*/
 
     }
 }
